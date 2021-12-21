cbb64ddb873ca963994d11dd86bf05be3a1e5e35
tsh <dr.tallin@gmail.com>
2021-12-21 22:11:18 +0200

    fix add

--- src/com/GTx_CS1332xII/MinHeap.java
+++ src/com/GTx_CS1332xII/MinHeap.java
@@ -1,5 +1,6 @@
 package com.GTx_CS1332xII;
 
+import java.util.Arrays;
 import java.util.NoSuchElementException;
 
 /**
@@ -54,9 +55,9 @@ public class MinHeap<T extends Comparable<? super T>> {
     }
 
     private void upheap(int startIndex){
-        for (int i=startIndex; i>0; i=i/2) {
+        for (int i=startIndex; i>1; i=i/2) {
             int parentIndex = i / 2;
-            if (backingArray[parentIndex].compareTo(backingArray[i]) < 0) {
+            if (backingArray[parentIndex].compareTo(backingArray[i]) > 0) {
                 swap(i, parentIndex);
             } else {
                 break;
@@ -65,8 +66,7 @@ public class MinHeap<T extends Comparable<? super T>> {
     }
 
     private void expandArray(){
-        T[] tmpArray = (T[]) new Object[backingArray.length * 2];
-
+        T[] tmpArray = (T[]) new Comparable[backingArray.length * 2];
         for (int i=1; i < backingArray.length; i++){
             tmpArray[i] = backingArray[i];
         }
@@ -136,3 +136,79 @@ public class MinHeap<T extends Comparable<? super T>> {
     }
 }
 
+/*
+============================================================
+MinHeap.java successfully compiled.
+============================================================
+Tests Passed: 2 / 29
+
+[Test Failure: remove] [-0.34] : NoSuchElementException not thrown when attempting to remove from an empty MinHeap.
+
+[Test Failure: remove] [-0.34] : This remove test was inconclusive due to: java.lang.NullPointerException
+
+[Test Failure: remove] [-0.34] : Unexpected content after removing 0 (the minimum element) from the MinHeap.
+	Before : [null, 0, 14, 7, null, null, null, null, null, null, null, null, null]
+	Expected : [null, 7, 14, null, null, null, null, null, null, null, null, null, null]
+	Actual : [null, 7, 14, 7, null, null, null, null, null, null, null, null, null]
+
+[Test Failure: remove] [-0.34] : Unexpected content after removing 0 (the minimum element) from the MinHeap.
+	Before : [null, 0, 7, 14, 21, 28, 35, 42, null, null, null, null, null]
+	Expected : [null, 7, 21, 14, 42, 28, 35, null, null, null, null, null, null]
+	Actual : [null, 42, 7, 14, 21, 28, 35, 42, null, null, null, null, null]
+
+[Test Failure: remove] [-0.34] : Unexpected content after removing 0 (the minimum element) from the MinHeap.
+	Before : [null, 0, 7, 14, 21, 28, 35, 42, 49, 56, null, null, null]
+	Expected : [null, 7, 21, 14, 49, 28, 35, 42, 56, null, null, null, null]
+	Actual : [null, 56, 7, 14, 21, 28, 35, 42, 49, 56, null, null, null]
+
+[Test Failure: remove] [-0.34] : Unexpected content after removing 0 (the minimum element) from the MinHeap.
+	Before : [null, 0, 7, 14, 42, 28, 35, 21, null, null, null, null, null]
+	Expected : [null, 7, 21, 14, 42, 28, 35, null, null, null, null, null, null]
+	Actual : [null, 21, 7, 14, 42, 28, 35, 21, null, null, null, null, null]
+
+[Test Failure: remove] [-0.34] : Unexpected content after removing 0 (the minimum element) from the MinHeap.
+	Before : [null, 0, 7, 14, 21, 28, 35, 42, 56, 49, null, null, null]
+	Expected : [null, 7, 21, 14, 49, 28, 35, 42, 56, null, null, null, null]
+	Actual : [null, 49, 7, 14, 21, 28, 35, 42, 56, 49, null, null, null]
+
+[Test Failure: remove] [-0.34] : Unexpected content after removing 0 (the minimum element) from the MinHeap.
+	Before : [null, 0, 14, 7, 42, 35, 28, 21, 56, 49, null, null, null]
+	Expected : [null, 7, 14, 21, 42, 35, 28, 49, 56, null, null, null, null]
+	Actual : [null, 49, 14, 7, 42, 35, 28, 21, 56, 49, null, null, null]
+
+[Test Failure: remove] [-0.34] : Unexpected content after removing 0 (the minimum element) from the MinHeap.
+	Before : [null, 0, 14, 7, 28, 21, 56, 49, 42, 35, null, null, null]
+	Expected : [null, 7, 14, 35, 28, 21, 56, 49, 42, null, null, null, null]
+	Actual : [null, 35, 14, 7, 28, 21, 56, 49, 42, 35, null, null, null]
+
+[Test Failure: remove] [-0.34] : Unexpected content after removing 0 (the minimum element) from the MinHeap.
+	Before : [null, 0, 14, 7, 28, 21, 56, 49, null, null, null, null, null]
+	Expected : [null, 7, 14, 49, 28, 21, 56, null, null, null, null, null, null]
+	Actual : [null, 49, 14, 7, 28, 21, 56, 49, null, null, null, null, null]
+
+[Test Failure: remove] [-0.34] : Unexpected content after removing 0 (the minimum element) from the MinHeap.
+	Before : [null, 0, 7, 14, 21, 28, 35, 42, 56, 49, 63, null, null]
+	Expected : [null, 7, 21, 14, 49, 28, 35, 42, 56, 63, null, null, null]
+	Actual : [null, 63, 7, 14, 21, 28, 35, 42, 56, 49, 63, null, null]
+
+[Test Failure: remove] [-0.34] : Unexpected content after removing 0 (the minimum element) from the MinHeap.
+	Before : [null, 0, 7, 14, 28, 21, 35, 49, 42, 56, 63, 70, null]
+	Expected : [null, 7, 21, 14, 28, 63, 35, 49, 42, 56, 70, null, null]
+	Actual : [null, 70, 7, 14, 28, 21, 35, 49, 42, 56, 63, 70, null]
+
+[Test Failure: remove] [-0.34] : Unexpected content after removing 0 (the minimum element) from the MinHeap.
+	Before : [null, 0, 7, 14, 35, 28, 42, 21, null, null, null, null, null]
+	Expected : [null, 7, 21, 14, 35, 28, 42, null, null, null, null, null, null]
+	Actual : [null, 21, 7, 14, 35, 28, 42, 21, null, null, null, null, null]
+
+[Test Failure: remove] [-0.34] : Unexpected content after removing 0 (the minimum element) from the MinHeap.
+	Before : [null, 0, 14, 7, 35, 28, 42, 21, null, null, null, null, null]
+	Expected : [null, 7, 14, 21, 35, 28, 42, null, null, null, null, null, null]
+	Actual : [null, 21, 14, 7, 35, 28, 42, 21, null, null, null, null, null]
+
+[Test Failure: validSize] [-0.34] : Size variable could not be validated for the following method(s) due to early test failure(s): add, remove.
+
+[Test Failure: validData] [-0.34] : Returned data could not be validated for the following method(s) due to early test failure(s): remove.
+
+[Test Failure: compareTo] [-0.34] : Correct compareTo() usage could not be validated for the following method(s) due to early test failure(s): remove, add.
+ */
