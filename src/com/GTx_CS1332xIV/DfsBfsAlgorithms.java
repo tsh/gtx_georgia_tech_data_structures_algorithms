9648c871469351448daad4bf4dc007300f8f53fb
Anton Nikulin <dr.tallin@gmail.com>
2022-04-24 14:19:11 +0300

    renamed Algorithms to avoid name conflicts

--- src/com/GTx_CS1332xIV/DfsBfsAlgorithms.java
+++ src/com/GTx_CS1332xIV/DfsBfsAlgorithms.java
@@ -1,19 +1,15 @@
 package GTx_CS1332xIV;
 
-import javax.swing.plaf.basic.BasicScrollPaneUI;
 import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import java.util.Queue;
-import java.util.Set;
 
 /**
  * Your implementation of various graph traversal algorithms.
  */
-public class GraphAlgorithms {
+public class DfsBfsAlgorithms {
 
     /**
      * Performs a breadth first search (bfs) on the input graph, starting at
